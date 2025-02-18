720b05b880f8adcca76ca4fcb328dd10459ea3d5
AngelGeoffroy1 <angelgeoffroy@outlook.fr>
2025-02-18 23:21:49 +0100

    Ajout du message de succès pour l'envoi d'email

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
 const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Servir les fichiers statiques depuis le répertoire courant
 app.use(express.static('./'));
+
+// Middleware pour analyser les requêtes POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Configuration de Nodemailer
 const transporter = nodemailer.createTransport({
